cc2d997d72f4047bcee26fb8912190bde48ecc4c
Gayathra Nakandala <ngayathra@gmail.com>
2018-10-19 18:16:08 +0530

    database class created

--- database/database.php
+++ database/database.php
@@ -1,1 +1,22 @@
-class
+<?php
+
+class Database{
+
+    const HOST = "localhost";
+    const USERNAME = "root";
+    const PASSWORD = "";
+    const DATABASE = "";
+
+    public function openConnection(){
+        $connection = new mysqli(self::HOST, self::USERNAME, self::PASSWORD, self::DATABASE);
+
+        if(!$connection){
+            die("Database connection failed " . mysqli_errno());
+        }else{
+            echo "Database connected";
+        }
+    }
+}
+
+$db = new Database();
+$db->openConnection();
